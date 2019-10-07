04d5fe99ca4a14f2cb1a190883d5a3c567dfcd4b
camsa mac <camsamac@132-147-208-248.xinuos.com>
2019-10-07 13:33:29 -0500

    Se agrego un boton que recorre y pinta los elementos de un arreglo, estos cambian cada vez que se clickea el boton

---
+++ FlutterApps/flutr_app/lib/Widget/myApp.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyButton extends StatefulWidget{
+  @override
+  _MyButtonState createState() => _MyButtonState();
+
+}
+
+class _MyButtonState extends State<MyButton>{
+
+  List<String> words = ['item1', 'item2', 'item3'];
+  String word="";
+  int index =0;
+
+  onPressedBoton(){
+    setState(() {
+      index = index < words.length-1 ? index+1 : 0;
+      word = words[index];
+    });
+
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    word = words[index];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stateful Widget",),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, //se centra en la pantalla,
+            children: <Widget>[
+              Text(word, style: TextStyle(
+                color: Colors.grey,
+                fontSize: 22.0,
+              ),),
+
+             RaisedButton(onPressed: onPressedBoton ,
+                 child:Text("Acualizar")
+             )
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- FlutterApps/flutr_app/lib/main.dart
+++ FlutterApps/flutr_app/lib/main.dart
@@ -1,32 +1,12 @@
+
 import 'package:flutter/material.dart';
+import 'Widget/myApp.dart';
 
 void main(){
-  var app = MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("flutApp"),
-      ),
-      body: Container(
-        child: Center(
-          child: Text("texfjs",
-          style: TextStyle(
-            fontSize: 46.0,//el font size acepta doubles
-          ),
-          ),
 
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.purple,
-      ),
-    ),
-  );
-
-  runApp(app);
+  runApp(MaterialApp(
+    home: MyButton(),
+  ));
 }
 
 
