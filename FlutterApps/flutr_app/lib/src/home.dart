47a6133531159013042aed05866fe923f859713b
camsa mac <camsamac@132-147-208-248.xinuos.com>
2019-10-07 16:41:35 -0500

    Se agrego un menu deslizable, con la clase Drawer, que resibe una funcion GetList que regresa un alista de items, con los estilos que proporciona la funcion getItem, este menu aun no tiene funcionalidad, solo se puede ver

--- FlutterApps/flutr_app/lib/src/home.dart
+++ FlutterApps/flutr_app/lib/src/home.dart
@@ -5,14 +5,37 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
+
 class _HomeState extends State<Home> {
+
+  //regresa el estilo de cada elemento
+  ListTile getItem (Icon icon, String descripcion){
+    return ListTile(
+      leading: icon,
+      title: Text(descripcion),
+    );
+  }
+
+  //Metodo que retorna la lista para el menu, con los estilos de getItem
+  ListView getList(){
+    return ListView(
+      children: <Widget>[
+
+        getItem(Icon(Icons.home), "Pagina Principal"),
+        getItem(Icon(Icons.settings), "configuracion"),
+        getItem(Icon(Icons.battery_charging_full), "Bateria")
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("PaginaPrincipal"),
          ),
-        drawer: Drawer(),//agrega el menu, si elementos 
+        //drawer: Drawer(),//agrega el menu, si elementos
+      drawer: Drawer(child:getList()),
     );
   }
 }
