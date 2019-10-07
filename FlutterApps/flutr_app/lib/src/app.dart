1782e4b8e1c24d9ad9788cf7ff09f3941ed13792
camsa mac <camsamac@132-147-208-248.xinuos.com>
2019-10-07 14:29:10 -0500

    Se creo un boton que te lleva a otra vista por medio del Navigation.push que tiene como parametros context y MaterialPageRoute

---
+++ FlutterApps/flutr_app/lib/src/app.dart
@@ -0,0 +1,33 @@
+import 'package:flutr_app/src/screens/second.dart';
+import 'package:flutter/material.dart';
+
+//esta tambien puede ser un statless
+
+class App extends StatefulWidget{
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Primer Pantalla"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Mostrar pantalla"),
+          onPressed: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Second()
+              )
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
